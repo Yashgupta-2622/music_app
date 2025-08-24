620bc1eeef37e83805dfd99dcd816ce823caaa9c
Yash Gupta <yashguptaalld@gmail.com>
2025-08-24 16:31:19 +0530

    feat(auth): persist auth state and fetch user data with JWT

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,12 +1,14 @@
 import uuid
 import bcrypt
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Header
 from database import get_db
+from middleware.auth_middleware import auth_middleware
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from pydantic_schemas.user_login import UserLogin
+import jwt
 
 router = APIRouter()
 
@@ -41,4 +43,17 @@ def login_user(user: UserLogin, db: Session = Depends(get_db)):
 
     if not is_match:
         raise HTTPException(400, 'Incorrect password!')
-    return user_db
+    
+    token = jwt.encode({'id': user_db.id}, 'password_key')
+
+    return {'token': token, 'user': user_db}
+
+@router.get('/')
+def current_user_data(db: Session=Depends(get_db), 
+                      user_dict = Depends(auth_middleware)):
+    user = db.query(User).filter(User.id == user_dict['uid']).first()
+
+    if not user:
+        raise HTTPException(404, 'User not found!')
+    
+    return user
