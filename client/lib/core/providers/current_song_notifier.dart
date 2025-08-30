b8d8798290427dea15ea8898ec18cdb568278fae
Yash Gupta <yashguptaalld@gmail.com>
2025-08-30 16:29:27 +0530

    feat(player): add music slab UI with play/pause and progress display

--- client/lib/core/providers/current_song_notifier.dart
+++ client/lib/core/providers/current_song_notifier.dart
@@ -6,6 +6,8 @@ part 'current_song_notifier.g.dart';
 @riverpod
 class CurrentSongNotifier extends _$CurrentSongNotifier {
   AudioPlayer? audioPlayer;
+  bool isPlaying = false;
+
   @override
   SongModel? build() {
     return null;
@@ -17,7 +19,28 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
     final audioSource = AudioSource.uri(Uri.parse(song.song_url));
     await audioPlayer!.setAudioSource(audioSource);
 
-    await audioPlayer!.play();
+    audioPlayer!.playerStateStream.listen((state) {
+      if (state.processingState == ProcessingState.completed) {
+        audioPlayer!.seek(Duration.zero);
+        audioPlayer!.pause();
+        isPlaying = false;
+
+        this.state = this.state?.copyWith(hex_code: this.state?.hex_code);
+      }
+    });
+
+    audioPlayer!.play();
+    isPlaying = true;
     state = song;
+  }
+
+  void playPause() {
+    if (isPlaying) {
+      audioPlayer?.pause();
+    } else {
+      audioPlayer?.play();
+    }
+    isPlaying = !isPlaying;
+    state = state?.copyWith(hex_code: state?.hex_code);
   }
 }
--- client/lib/features/home/view/pages/home_page.dart
+++ client/lib/features/home/view/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/features/home/view/pages/library_page.dart';
 import 'package:client/features/home/view/pages/songs_page.dart';
+import 'package:client/features/home/view/widgets/music_slab.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,15 +15,17 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   int selectedIndex = 0;
 
-  final pages = const [
-    SongsPage(),
-    LibraryPage(),
-  ];
+  final pages = const [SongsPage(), LibraryPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[selectedIndex],
+      body: Stack(
+        children: [
+          pages[selectedIndex],
+          const Positioned(bottom: 0, child: MusicSlab()),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
         onTap: (value) {
